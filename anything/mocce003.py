7d3bb265016fa67692355fee859df95125f7a02b
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2022-04-26 20:15:57 -0300

    Update mocce003.py

--- anything/mocce003.py
+++ anything/mocce003.py
@@ -1,2 +1,63 @@
-import pyautogui as autoGay
+from asyncio.windows_events import NULL
+import time
+import pyautogui as pa
 
+
+def getIp():
+    pa.hotkey('win', 'r')
+    pa.write('cmd')
+    pa.press('enter')
+    time.sleep(0.5)
+    pa.write('ipconfig')
+    pa.press('enter')
+    time.sleep(0.5)
+    pa.click(1366/2, 768/2)
+    pa.hotkey('ctrl', 'a')
+    pa.hotkey('ctrl', 'c')
+    time.sleep(0.5)
+    pa.hotkey('alt', 'f4')
+    time.sleep(0.5)
+
+def saveOnNotepad(cont):
+    pa.hotkey('win', 'r')
+    pa.write('notepad')
+    pa.press('enter')
+    time.sleep(0.5)
+    pa.hotkey('ctrl', 'v')
+    pa.hotkey('ctrl', 's')
+    pa.write('ip-do-boboca{}.txt' .format(cont))
+    pa.press('enter')
+    time.sleep(0.5)
+    pa.hotkey('alt', 'f4')
+    time.sleep(0.5)
+
+def saveAScreenshot(cont):
+    pa.press('prntscrn')
+    pa.hotkey('win', 'r')
+    pa.write('mspaint')
+    pa.press('enter')
+    time.sleep(0.5)
+    pa.hotkey('ctrl', 'v')
+    time.sleep(1)
+    pa.hotkey('ctrl', 's')
+    time.sleep(0.5)
+    pa.write('print_do_boboca{}.jpeg' .format(cont))
+    pa.press('enter')
+    time.sleep(0.5)
+    pa.hotkey('alt', 'f4')
+    time.sleep(0.5)
+
+cont1 = 0
+cont2 = 0
+while True:
+    escolha = pa.confirm(text='Escolha o que deseja fazer', title='App legal!', buttons=['Salvar ip no bloco de notas', 'Tirar um print', 'Parar'])
+    print(escolha)
+    if escolha == 'Salvar ip no bloco de notas':
+        getIp()
+        saveOnNotepad(cont1)
+        cont1 += 1
+    elif escolha == 'Tirar um print':
+        saveAScreenshot(cont2)
+        cont2 += 1
+    elif escolha == 'Parar' or escolha == None:
+        break
